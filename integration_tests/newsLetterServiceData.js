4cc048932c6c9d04249555fc3d6e9b71926e520e
Hazsni <mozerhajni@gmail.com>
2023-02-23 21:05:59 +0100

    mod

--- integration_tests/newsLetterServiceData.js
+++
@@ -1,8 +0,0 @@
-export const mockData = {
-    email: 'johndoe@gth.com',
-}
-
-export const mockResponseData = {
-    _id: 1,
-    email: 'johndoe@gth.com',
-}
--- src/__mocks__/config.js
+++
@@ -1,4 +0,0 @@
-export default {
-    token_key: 'NotSoSecretTokenKeyForTestingOnly',
-  };
-  
